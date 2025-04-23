886752193e67b283d19ea8880c821f9a2967e7c3
airrraaaiii <airaaugustina81@gmail.com>
2025-04-23 14:51:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("woww apa ini")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Let's start building! For wowww apa ini to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
