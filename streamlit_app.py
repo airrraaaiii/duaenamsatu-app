256bb8af5b638165fa61b1923768aa059f92ca6f
airrraaaiii <airaaugustina81@gmail.com>
2025-04-23 15:41:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("aiirraaaa")
 st.write(
     "Let's start building! For wowww apa ini to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.image("C23D9731-D528-41C0-9A94-E01DF5001ACB.jpeg")
