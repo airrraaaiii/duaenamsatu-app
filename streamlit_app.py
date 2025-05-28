a5b3fdafa73c31ce367ec9fa05bedda22c6ead79
airrraaaiii <airaaugustina81@gmail.com>
2025-05-28 13:35:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,25 @@ st.title("aiirraaaa")
 st.write(
     "Let's start building! For wowww apa ini to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-st.image("C23D9731-D528-41C0-9A94-E01DF5001ACB.jpeg")
+st.image("IMG_3576.jpeg")
+st.write("\n")
+st.subheader("aira paskib keren mantap")
+st.write(
+    "lagi sering dispen gara gara paskib"
+)
+st.write(
+    """
+    - sayang ale
+    - sayang keifa
+    - sayang rin
+    - sayang tari
+    - sayang maudi
+    """
+)
+st.header("ngecheck nilai ganjil genap")
+angka = st.number_input("kasih angka:", value=0, step=1)
+
+if(angka % 2) == 0:
+    st.write(f"{angka} adalah genap")
+else:
+    st.write(f"{angka} adalah ganjil")
